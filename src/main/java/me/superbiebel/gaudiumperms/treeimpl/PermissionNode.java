623872b3c80e878a58daf78b14c638699ccf6b4f
superbiebel <superbiebel@gmail.com>
2021-09-30 13:02:37 +0200

    indev

--- src/main/java/me/superbiebel/gaudiumperms/treeimpl/PermissionNode.java
+++ src/main/java/me/superbiebel/gaudiumperms/treeimpl/PermissionNode.java
@@ -1,5 +1,6 @@
 package me.superbiebel.gaudiumperms.treeimpl;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.Getter;
@@ -10,7 +11,7 @@ public class PermissionNode {
     @Getter
     private final boolean value;
     @Getter
-    private final Set<PermissionNode> children = new HashSet<>();
+    private final HashMap<String, PermissionNode> children = new HashMap<>();
 
     public PermissionNode(String name, boolean value) {
         this.name = name;
@@ -18,10 +19,10 @@ public class PermissionNode {
     }
 
     public void addChild(PermissionNode child) {
-        children.add(child);
+        children.put(child.getName(),child);
     }
     public boolean removeChild(PermissionNode child) {
-        return children.remove(child);
+        return children.remove(child.getName()) != null;
     }
     public boolean hasChildren() {
         return children.isEmpty();
